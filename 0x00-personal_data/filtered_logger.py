90ae49638de9dcd7e098875c9d6bbd94b9173e76
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-27 21:09:54 +0300

    filtered

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -61,9 +61,9 @@ def get_logger() -> logging.Logger:
 def get_db() -> mysql.connector.connection.MYSQLConnection:
     """ Function that returns a connector to the database
     """
-    database = mysql.connector.connection.MSQLConnection(
-        user_name=environ.get('PERSONAL_DATA_USERNAME', 'root'),
+    db = mysql.connector.connection.MSQLConnection(
+        user=environ.get('PERSONAL_DATA_USERNAME', 'root'),
         password=environ.get('PERSONAL_DATA_DB_PASSWORD', ''),
         host=environ.get('PERSONAL_DATA_DB_HOST', 'localhost'),
-        database_name=environ.get('PERSONAL_DATA_DB_NAME'))
-    return database
+        database=environ.get('PERSONAL_DATA_DB_NAME'))
+    return db
