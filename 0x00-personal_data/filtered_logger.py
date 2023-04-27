8ab0bd26491f146e371215aea3dde2cc4af7e61c
Awet11-Tesfay <hailegb2021@gmail.com>
2023-04-27 18:08:36 +0300

    filtered

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 """
 
 from typing import List
+from logging import Formatter
+from logging import Logger
 import logging
 import re
 import mysql.connector
@@ -19,3 +21,25 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{y}=.*?{separator}',
                          f'{y}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """ initialized method
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Filter values in incomming log records
+        """
+        return filter_datum(self.fiels, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
