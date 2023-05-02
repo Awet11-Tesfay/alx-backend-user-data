fc853ad41dcf10a0f7168b2ff1414ad9d0040d91
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-02 21:24:21 +0300

    'basic'

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -36,6 +36,6 @@ def not_found(error) -> str:
 
 
 if __name__ == "__main__":
-    host = getenv("API_HOST", "127.0.0.1")
+    host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
