fd5a9b082771002b585e2459e0f2584b496c1b05
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-02 19:53:11 +0300

    'basic'

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,6 +21,13 @@ def unauthorized(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "127.0.0.1")
     port = getenv("API_PORT", "5000")
