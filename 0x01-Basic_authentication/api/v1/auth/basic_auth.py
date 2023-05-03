aae2d907b4eb44ebcc59bf268bc4cb00a5771387
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-04 00:35:16 +0300

    'auth'

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Basic auth
+"""
+from base64 import b64encode
+from api.v1.auth.auth import Auth
+from typing import TypeVar
+from models.user import User
+
+class BasicAuth(Auth):
+    """ Class inherits from the auth class
+    """
