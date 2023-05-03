ccec35cee7da77e845e617b96de74ebeed343e6a
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-04 00:59:28 +0300

    'auth'

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,3 +10,15 @@ from models.user import User
 class BasicAuth(Auth):
     """ Class inherits from the auth class
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Basic base64 part
+        """
+        if authorization_header is None:
+            return None
+        if authorization_header is not str:
+            return None
+        if authorization_header.startwith("Basic"):
+            return True
+
+        encoded = authorization_header.split('', 1)[1]
