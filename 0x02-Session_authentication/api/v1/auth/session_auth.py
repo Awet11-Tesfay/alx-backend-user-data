a861ad37fa77d3fd68ba9d0bf896190db2cf2710
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-04 21:29:38 +0300

    'session_auth'

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ Session class that inherits from Auth
 """
-from api.v1.auth.auth import Auth
+from .auth import Auth
 from uuid import uuid4
 from models.user import User
 
 
 class SessionAuth(Auth):
-    """ That inherits from Auth
+    """ That inherits from the previous
     """
