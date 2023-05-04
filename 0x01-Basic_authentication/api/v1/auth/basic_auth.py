95ca41fd2fe8b678b32c87b29221ac0e94bf3b96
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-04 08:54:12 +0300

    'auth'

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -56,4 +56,4 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
 
-        return tuple(decoded_base64_authorization_header.split(':', 1))
+        return decoded_base64_authorization_header.split(':', 1)
