0edeffad5d066b0002cd991f46563893e2648174
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-04 22:28:32 +0300

    'session_id'

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,3 +9,11 @@ from models.user import User
 class SessionAuth(Auth):
     """ That inherits from the previous
     """
+    def user_id_for_session(self, session_id: str = None) -> str:
+        """ returns a user ID based on a session ID
+        """
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+        return SessionAuth.user_id_by_session_id.get(session_id)
