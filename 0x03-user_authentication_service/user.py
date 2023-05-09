e619ea7a4e58de9485fd076c28f106ed7734da4a
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-09 08:51:25 +0300

    'user_model'

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Create a SQLAlchemy model named User for database named users
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy .ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ The main class
+    """
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(Integer, nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
