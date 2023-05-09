ebafed640a15a5f85309df0f708c5d35a70958c5
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-09 10:44:58 +0300

    'create_model'

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -13,31 +13,31 @@ from user import Base
 from user import Base
 
 
-class Db:
-    """ Db class
+class DB:
+    """DB class
     """
 
     def __init__(self) -> None:
-        """ Initialize a new DB instance
+        """Initialize a new DB instance
         """
-        self._engine = create_engine("sqllite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
-        Base.metadate.create_all(self._engine)
+        Base.metadata.create_all(self._engine)
         self.__session = None
 
-        @property
-        def _session(self) -> Session:
-            """ Memorized session object
-            """
-            if self.__session is None:
-                DBSession = sessionmaker(bind=self._engine)
-                self.__session = DBSession()
-                return self.__session
-            
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
         def add_user(self, email: str, hash_password: str) -> User:
             """ Returns User object
             """
-            user = User(email=email, hash_password=hash_password)
+            user = User(email=email, hashed_password=hashed_password)
             self._session.add(user)
             self._session.commit()
             return user
