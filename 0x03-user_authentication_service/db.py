9a6685feb50cecfb15e4ada543f4c6670d88b632
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-10 16:58:19 +0300

    create_user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 from user import User
 from typing import TypeVar
@@ -18,9 +19,9 @@ class DB:
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
+        """ Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -34,10 +35,10 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-        def add_user(self, email: str, hash_password: str) -> User:
-            """ Returns User object
-            """
-            user = User(email=email, hashed_password=hashed_password)
-            self._session.add(user)
-            self._session.commit()
-            return user
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """ Returns User object
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
