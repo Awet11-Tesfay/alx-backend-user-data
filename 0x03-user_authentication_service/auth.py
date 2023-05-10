5d9757386220cb11ed3655a200bae86231417675
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-10 18:51:52 +0300

    hash_password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" This file is for Authentication of the user
+"""
+from bcrypt import hashpw, gensalt
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """ Takes password arguments and return bytes
+    """
+    return bcrypt.hashpw(password.encode('Utf-8'), gensalt)
