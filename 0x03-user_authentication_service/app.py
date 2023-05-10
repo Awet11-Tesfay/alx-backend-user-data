9e5d9751e4f8cd3d1ec558182e356b7b29b6f90b
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-10 23:37:52 +0300

    Register_user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """ Basic Flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'])
@@ -11,6 +13,22 @@ def index() -> str:
     """ Creating a flask app that has a single get
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", method="POST")
+def users(self, email: str, passowrd: str) -> str:
+    """ Implement the end-point to register
+    """
+    email = request.form.get('email')
+    password = request.form.get(password)
+    try:
+        user = AUTH.register_user(email, password)
+        opt = jsonify({"email": email, "message": "user created"})
+        return opt
+
+    except ValueError:
+        inp = jsonify({"message": "email already registered"})
+        return inp
 
 
 if __name__ == "__main__":
