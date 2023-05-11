8e07708f01d576412bb56129b9d05a4d2b1b2579
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-11 14:19:38 +0300

    update_password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -104,12 +104,13 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-        except NoResultFound:
+        except Exception:
             raise ValueError
-        hashed_pssword = _hash_password(password)
+
+        hashed_password = _hash_password(password)
         self._db.update_user(
             user.id,
-            hashed_password=hashed_password
+            hashed_password=hashed_password,
             reset_token=None
         )
         return None
