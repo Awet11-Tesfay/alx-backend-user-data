f460dfc24bc45eeac85ca927f225e69300ce1b4d
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-11 13:14:02 +0300

    reset_pass_token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -93,9 +93,9 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            reset_token = _generate_uuid()
-            self._db.update_user(user.id, reset_token=reset_token)
-            return reset_token
+
         except NoResultFound:
             raise ValueError()
         
+        user.reset_token = _generate_uuid()
+        return user.reset_token
