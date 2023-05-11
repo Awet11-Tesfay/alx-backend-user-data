f81daeb9ea392bca2fc98d82d3977395f220f178
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-11 15:50:12 +0300

    update_pwd_end_point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -95,10 +95,9 @@ def update_password() -> str:
 
     try:
         AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
     except Exception:
         abort(403)
-
-    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
