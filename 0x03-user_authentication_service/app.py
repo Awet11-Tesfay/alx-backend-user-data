274cfcd82136d7ff77cef76dc25c7be73808fa36
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-11 11:41:34 +0300

    login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -38,15 +38,12 @@ def login() -> str:
     """
     email = request.form.get('email')
     password = request.form.get('password')
-    valid_login = AUTH.valid_login(email, password)
-
-    if not valid_login:
-        abort(401)
-
-    session_id = AUTH.create_session(email)
-    response = jsonify({'email': email, 'message': 'logged in'})
-    response.set_cookie('session_id', session_id)
-    return response
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        res = jsonify({"email": f'{email}', "message": "logged in"})
+        res.set_cookie("session_id", session_id)
+        return res
+    abort(401)
 
 
 if __name__ == "__main__":
