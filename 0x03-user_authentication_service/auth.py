cd31e45c943ca88b89ddc72352ce8bfb627cdd01
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-11 11:02:09 +0300

    create_session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,3 +56,14 @@ class Auth:
             return True
         else:
             return False
+
+    def create_session(self, email: str) -> str:
+        """ Implementing create_session takes string arguments return session id
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
