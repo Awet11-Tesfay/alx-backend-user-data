80ecfd23437b9ed20a296df5c1ccab9cba07a9b4
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-11 12:05:40 +0300

    destroy_session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,3 +79,11 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Takes user_id and returns None
+        """
+        if user_id:
+            return self._db.update_user(user_id, session_id=None)
+        else:
+            return None
