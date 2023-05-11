1af3341cb400e6418debf0d151483242cea6081e
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-11 12:17:51 +0300

    log_out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Basic Flask app
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 from typing import Tuple, Union
 
@@ -47,6 +47,18 @@ def login() -> str:
     else:
         abort(401)
 
+
+@app.route('/sessions', methods='DELETE')
+def logout():
+    """ Function to respond to the Delete/Session route
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
+    else:
+        abort(403)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port='5000')
