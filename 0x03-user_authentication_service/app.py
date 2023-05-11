28d325d90adb51a96a5524aa79105a8e32e58685
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-11 15:48:28 +0300

    update_pws_end_point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -85,7 +85,7 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
-@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+@app.route('/reset_password', methods=['PUT'])
 def update_password() -> str:
     """ Function in the app module to respond put/reset_password
     """
@@ -98,7 +98,7 @@ def update_password() -> str:
     except Exception:
         abort(403)
 
-    return jsonify({"email": f'{email}', "message": "password updated"}), 200
+    return jsonify({"email": email, "message": "Password updated"}), 200
 
 
 if __name__ == "__main__":
