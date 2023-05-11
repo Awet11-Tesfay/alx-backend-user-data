b9ee2886c0b0a9486260fadaeffe4844f0179856
Awet11-Tesfay <hailegb2021@gmail.com>
2023-05-11 12:20:11 +0300

    log_out

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,8 +57,8 @@ def logout():
     if user:
         AUTH.destroy_session(user.id)
         return redirect('/')
-    else:
-        abort(403)
+    abort(403)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port='5000')
